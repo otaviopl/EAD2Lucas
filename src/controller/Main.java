8ad88cf64a0e660d730d4fe64aae39960004fcef
otaviopl <otavio.pereira.lopes@gmail.com>
2024-04-15 23:54:10 -0300

    FEAT: main running and with clean code

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,17 +1,40 @@
 package controller;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import prod.IntercalateArrays;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Digite o tamanho dos vetores:");
+        int N = scanner.nextInt();
+
+        int[] A = readArray(scanner, N, "A");
+        int[] B = readArray(scanner, N, "B");
+
+        int[] C = IntercalateArrays.intercalateArrays(A, B);
+
+        System.out.println("Vetor resultante após intercalação:");
+        printArray(C);
+
+        scanner.close();
+    }
+
+    private static int[] readArray(Scanner scanner, int size, String name) {
+        System.out.println("Digite os elementos do vetor " + name + ":");
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
         }
+        return array;
+    }
+
+    private static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
