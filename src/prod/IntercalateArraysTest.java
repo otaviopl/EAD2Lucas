12058f96d107519d98b267b6f6ba7a417bb22d08
otaviopl <otavio.pereira.lopes@gmail.com>
2024-04-15 23:44:12 -0300

    TEST: add the first idea of tests

---
+++ src/prod/IntercalateArraysTest.java
@@ -0,0 +1,23 @@
+package prod;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+public class IntercalateArraysTest {
+
+    @Test
+    public void testIntercalateArrays() {
+        int[] A = {5, 10, 20, 30, 40, 50};
+        int[] B = {6, 3, 8, 2, 0, 60};
+        int[] expected = {5, 6, 10, 3, 20, 8, 30, 2, 40, 0, 50, 60};
+        assertArrayEquals(expected,IntercalateArrays.intercalateArrays(A, B));
+    }
+
+    @Test
+    public void testIntercalateArraysDifferentLengths() {
+        int[] A = {1, 1, 1};
+        int[] B = {1, 2, 3};
+        int[] expected = {1, 1, 1, 2, 1, 3};
+        assertArrayEquals(expected, IntercalateArrays.intercalateArrays(A, B));
+    }
+}
